d4d0818d77b38b1cb73f7a35f00a8daf48969cbc
larra22 <larra22@gmail.com>
2023-07-26 12:22:43 +0200

    nombre cambiado en pm2

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'nombre_de_tu_app',
+        name: 'roadmap',
         script: 'npm',
         args: 'run dev -- --host',
         watch: '.',
